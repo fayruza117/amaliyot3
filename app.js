0790f1d3141fb7a50a132cdc4a7fd12a295713d8
fayruza117 <fbobomurodova073@gmail.com>
2023-10-26 05:49:53 +0300

    a

---
+++ app.js
@@ -0,0 +1,76 @@
+let mevalar = ["Kiwi", "Lemon", "Peach", "apple", "cherry"];
+console.log(mevalar);
+let meva = prompt("Meva qo'shing");
+fruits.push(meva);
+console.log(mevalar);
+fruits.shift();
+console.log(mevalar);
+
+
+// let str = "Lorem IPSUM dolor sit ipsum amet. +998989832"  
+
+// console.log(str.length);
+// console.log(str.charAt(3));
+// console.log(str.charCodeAt(22));
+// console.log(str.concat
+// ("Eshmat","Toshmat"));
+// console.log(str.endsWith("amet."));
+// console.log(str.includes("j"));
+// console.log(str.indexOf("a"));
+// console.log(str.lastIndexOf("a"));
+// console.log(str.padEnd(10,"0"));
+// console.log(str.padStart(10,"0"));
+// console.log(str.repeat(3));
+// console.log(str.replace(" ", " "));
+// console.log(str.padStart(10,"0"));
+// console.log(str.repeat(3));
+// console.log(str.replace(" ", " "));
+// console.log(str.replaceAll(" ", ","));
+// console.log(str.search(/\ +998*9*/)); // string va regex
+// console.log(str.slice(14));
+// console.log(str.split(" "));
+// console.log(str.startsWith("lorem"));
+// console.log(str.toLowerCase());
+// console.log(str.toUpperCase());
+// console.log(str.trim());
+// console.log(str.trimStart());
+// console.log(str.trimEnd());
+
+// Array methods
+
+// let fruits = ["Apple", "Lime", "Banana", "Cherry", "Strawberry", "Water melon"];
+
+// console.log(fruit.length);
+// console.log(fruit.concat(["Pear", "Lime"], ["Strawberry", "Water melon"], ["Pinapple"]));
+// console.log(fruit.flat(2));
+// console.log(fruit.includes("apple"));
+// console.log(fruit.indexOf("Apple"));
+// console.log(fruit.lastIndexOf("Apple"));
+// console.log(fruit.join(" -> "));
+// console.log(fruit.reverse());
+// console.log("fruits".split("").reverse().join(""));
+// console.log(fruit.slice(1, 3));
+// console.log(fruit.sort());
+// console.log(fruit.sort().reverse());
+
+// fruits.push("Strawberry");
+// fruits.unshift("Strawberry");
+// fruits.pop();
+// fruits.shift();
+// fruits.unshift(2, 0, "Pear", "Pineapple");
+// fruits.splice(fruits.length, 0, "Pear"); // push
+// fruits.splice(0, 0, "Pear"); // unshift
+// fruits.splice(fruits.length - 1, 1); // pop
+// fruits.splice(0, 1); //shift
+
+// console.log(fruits);
+
+
+
+
+
+
+
+
+
+
